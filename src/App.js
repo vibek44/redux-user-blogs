8656bb7dfee65a788c454cc67135cafe39659a4c
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-28 16:30:27 +0200

    importing useSelector and using it to access state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react"
+import { useSelector, useDispatch } from "react-redux"
 const App = () => {
-  return (
-    <>
-      <p>hello</p>
-    </>
-  )
+  const notification = useSelector((state) => state)
+  console.log(notification)
+
+  return <div>hi there</div>
 }
 
 export default App
