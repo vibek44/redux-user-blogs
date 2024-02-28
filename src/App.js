93f0afd00b12c92c513270f430aa1ba7367d7ed4
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-28 16:50:33 +0200

    importing and using Notification component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react"
-import { useSelector, useDispatch } from "react-redux"
+import Notification from "./components/Notification"
 const App = () => {
-  const notification = useSelector((state) => state)
-  console.log(notification)
-
-  return <div>hi there</div>
+  return (
+    <>
+      <Notification />
+    </>
+  )
 }
 
 export default App
