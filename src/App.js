33c9a82f2084092920c27030e26b55f54c2567c4
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-29 20:42:49 +0200

    importing BrowserRouter,Routes,Route fro react-router-dom and using it to handle redirection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react"
-import Notification from "./components/Notification"
+import React from 'react'
+import Notification from './components/Notification'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import MenuLink from './components/MenuLink'
+import Home from './components/Home'
+import Footer from './components/Footer'
+import { useUser } from './customHooks/index'
 const App = () => {
   return (
-    <>
+    <div>
       <Notification />
-    </>
+      <h2>Bloglist App</h2>
+      <Router>
+        <MenuLink />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </div>
   )
 }
 
