752addb5ada4646bf844ebfa33573e37fe5df425
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-29 20:39:14 +0200

    adding Notification component

---
+++ src/components/Notification.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { useSelector, useDispatch } from 'react-redux'
+
+const Notification = () => {
+  const notification = useSelector((state) => state.notification)
+  if (!notification) return null
+
+  return <p>{notification}</p>
+}
+
+export default Notification
