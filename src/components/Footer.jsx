687461dffbc55cc28d0553e6f6ab52eaa2e0abbe
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-29 20:38:02 +0200

    adding footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      Bloglist app for | <a href='http://localhost:3000'>blog-user</a>. See{' '}
+      <a href='https://github.com/vibek44/redux-user-blogs/blob/main/src/index.js'>
+        https://github.com/vibek44/redux-user-blogs
+      </a>{' '}
+      for the source code.
+    </div>
+  )
+}
+
+export default Footer
