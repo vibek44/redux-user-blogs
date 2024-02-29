c8e86a12cdf7f613031b452b79d4ac7733cbe0e1
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-29 20:41:09 +0200

    importing index.css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
-import { Provider } from "react-redux"
-import store from "../store"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import App from './App'
+import store from '../store'
+import './css/index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
   </Provider>
