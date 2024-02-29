d4ad2e8c66f8765adbed0bb8fbf329ba5a4f94db
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-29 20:52:41 +0200

    deleted and created new file

--- src/components/Notification.js
+++
@@ -1,9 +0,0 @@
-import { useSelector, useDispatch } from "react-redux"
-const Notification = () => {
-  const notification = useSelector((state) => state.notification)
-  if (!notification) return null
-
-  return <p>{notification}</p>
-}
-
-export default Notification
