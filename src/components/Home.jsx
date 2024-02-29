d139e19f360570d283f669c52ccf8a4a9226df43
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-29 20:37:41 +0200

    adding Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to Bloglist App</h2>
+      <p>
+        login to
+        <em> continue</em>
+      </p>
+    </div>
+  )
+}
+
+export default Home
