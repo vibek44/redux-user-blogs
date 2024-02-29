b2cccb9fed25c9c209ff67ef0011a74d69c2b116
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-29 20:46:24 +0200

    adding webpack module loader style-loader and css-loade

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,34 @@
-const path = require("path");
+const path = require('path')
 
-const config = () => {
-  return {
-    entry: "./src/index.js",
-    output: {
-      path: path.resolve(__dirname, "build"),
-      filename: main.js,
-    },
-  };
-};
+const config = () => ({
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.js',
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'build'),
+    compress: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react', '@babel/preset-env'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+})
+
+module.exports = config
