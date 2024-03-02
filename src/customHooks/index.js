a5d344d85df2cc0a96c462a11c5cc61dac270867
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-03-02 18:20:34 +0200

    adding useField custom hook for login form

--- src/customHooks/index.js
+++ src/customHooks/index.js
@@ -4,5 +4,15 @@ import { useState } from 'react'
 export const useUser = () => {
   const [user, setUser] = useState(null)
 
-  return user
+  return { user }
+}
+
+export const useField = () => {
+  const [value, setValue] = useState('')
+
+  return {
+    type,
+    value,
+    onChange,
+  }
 }
