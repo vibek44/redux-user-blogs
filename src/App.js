9f041b148151cd4dd466f583f12199d57aa21d83
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-03-02 18:23:56 +0200

    Creating Route for different url or path and rendering view accoringly

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React from 'react'
 import Notification from './components/Notification'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import MenuLink from './components/MenuLink'
 import Home from './components/Home'
+import Users from './components/Users'
+import Blogs from './components/Blogs'
+import LoginForm from './components/LoginForm'
 import Footer from './components/Footer'
 import { useUser } from './customHooks/index'
+
 const App = () => {
+  const { user } = useUser()
+
   return (
     <div>
       <Notification />
       <h2>Bloglist App</h2>
-      <Router>
-        <MenuLink />
-        <Routes>
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </Router>
+
+      <MenuLink user={user} />
+      <Routes>
+        <Route
+          path='/users'
+          element={user ? <Users /> : <Navigate replace to='/login' />}
+        />
+        <Route path='/login' element={<LoginForm />} />
+        <Route path='/blogs' element={<Blogs />}></Route>
+        <Route path='/' element={<Home />} />
+      </Routes>
       <Footer />
     </div>
   )
