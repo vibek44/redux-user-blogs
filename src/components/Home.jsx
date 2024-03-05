e16317e693f023f01783f153f6568b647e865868
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-03-05 18:18:18 +0200

    adding conditional rendering of home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Home = () => {
+const Home = ({ user }) => {
+  if (user)
+    return (
+      <div>
+        <p>welcome {user.username} to App Bloglist</p>
+        <em>
+          please continue to search for blogs and <b>create</b> your own
+        </em>
+      </div>
+    )
+
   return (
     <div>
       <h2>Welcome to Bloglist App</h2>
