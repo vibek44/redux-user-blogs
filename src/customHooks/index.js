49548fa75d5af171eda57b29e3095ffb77436e7b
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-03-05 18:23:45 +0200

    adding error handler and navigating url according using useNavigate

--- src/customHooks/index.js
+++ src/customHooks/index.js
@@ -1,14 +1,39 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { UseDispatch, useDispatch } from 'react-redux'
+import { setNotification } from '../reducers/notificationReducer'
+import { useNavigate } from 'react-router-dom'
+const baseUrl = 'http://localhost:3003/api/login'
 
-export const useUser = () => {
+export const useUser = (credential) => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
   const [user, setUser] = useState(null)
 
-  return { user }
+  useEffect(() => {
+    if (credential) {
+      axios
+        .post(baseUrl, credential)
+        .then((res) => {
+          //console.log(res.data)
+          setUser(res.data)
+        })
+        .catch((error) => {
+          dispatch(setNotification(error.response.data.error))
+          navigate('/login')
+        })
+    }
+  }, [credential])
+
+  return user
 }
 
-export const useField = () => {
+export const useField = (type) => {
   const [value, setValue] = useState('')
+
+  const onChange = (e) => {
+    setValue(e.target.value)
+  }
 
   return {
     type,
