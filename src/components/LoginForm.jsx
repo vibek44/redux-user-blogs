778561427468e8b1c265887f394d8681e66e56a8
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-03-05 18:20:35 +0200

    destructuring handleLogin and using it to for login functionality

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,31 +1,37 @@
 import React from 'react'
 import { useDispatch /*useSelector*/ } from 'react-redux'
 import { setNotification } from '../reducers/notificationReducer'
+import { useField } from '../customHooks'
+import { useNavigate } from 'react-router-dom'
 
-const LoginForm = () => {
+const LoginForm = ({ handleLogin }) => {
+  const navigate = useNavigate()
   const dispatch = useDispatch()
+  const userName = useField('text')
+  const passWord = useField('password')
+
   const handleSubmit = async (e) => {
     e.preventDefault()
-    const username = e.target.username.value
-    const password = e.target.password.value
-    if (!(username && password))
-      dispatch(setNotification('username or password is missing'))
-
-    try {
-    } catch (error) {}
-    //console.log(e.target.username.value, e.target.password.value)
+    if (!(userName.value && passWord.value))
+      return dispatch(setNotification('username or password is missing'))
+    const credentials = {
+      username: userName.value,
+      password: passWord.value,
+    }
+    handleLogin(credentials)
   }
+
   return (
     <form onSubmit={handleSubmit}>
       <fieldset>
         <legend>Login</legend>
         <div>
           username
-          <input name='username' type='text' />
+          <input {...userName} />
         </div>
         <div>
           password
-          <input name='password' type='password' />
+          <input {...passWord} />
         </div>
         <button name='submitbutton' type='submit'>
           login
