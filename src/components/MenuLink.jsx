8cbe833540b4300e0da99d8e492487dd365570ab
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-03-06 16:48:20 +0200

    adding logout button functionality

--- src/components/MenuLink.jsx
+++ src/components/MenuLink.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const MenuLink = ({ user }) => {
+const MenuLink = ({ user, handleLogOut }) => {
   const Style = {
     paddingRight: 5,
   }
+
   return (
     <div>
       <Link style={Style} to='/'>
@@ -17,7 +18,10 @@ const MenuLink = ({ user }) => {
         users
       </Link>
       {user ? (
-        <em>{user.username} logged in</em>
+        <>
+          <em>{user.username} logged in</em>
+          <button onClick={handleLogOut}>Logout</button>
+        </>
       ) : (
         <Link style={Style} to='/login'>
           login
