26aa62f1f77be2cacbe7dfe57726d0f114b7005b
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-03-06 18:21:44 +0200

    changing default export to name export

--- src/requestService/user.js
+++ src/requestService/user.js
@@ -2,9 +2,7 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3003/api/login'
 
-const verifyUser = async (credential) => {
+export const verifyUser = async (credential) => {
   const request = await axios.post(baseUrl, credential)
   return request.data
 }
-
-export default { verifyUser }
