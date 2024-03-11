ce0e4f2513e6bda7385ac2609b750c29b3a5743d
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-03-11 16:22:37 +0200

    adding userReducer

--- store.js
+++ store.js
@@ -1,9 +1,13 @@
-import { configureStore } from "@reduxjs/toolkit"
-import notificationReducer from "./src/reducers/notificationReducer"
+import { configureStore } from '@reduxjs/toolkit'
+import notificationReducer from './src/reducers/notificationReducer'
+import blogReducer from './src/reducers/blogReducer'
+import userReducer from './src/reducers/userReducer'
 
 const store = configureStore({
   reducer: {
     notification: notificationReducer,
+    blogs: blogReducer,
+    user: userReducer,
   },
 })
 
