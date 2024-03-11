60fcc2c769e6cd0eedc2bc7b64e1eab073c7d740
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-03-11 16:18:08 +0200

    destructuring props and using handleLike and handleRemove

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
 
-const Blogs = ({ user }) => {
-  if (user) return <p>welcome {user.username}</p>
+const Blogs = ({ user, blogs, handleLike, handleRemove }) => {
+  if (user && blogs.length > 0)
+    return (
+      <>
+        <h3>
+          welcome <em>{user.username}</em>
+        </h3>
+        <h4>BlogList</h4>
+        {blogs.map((blog) => (
+          <div key={blog.id}>
+            <p>
+              {blog.title}
+              <button onClick={() => handleLike(blog)}>like</button>
+              <button onClick={() => handleRemove(blog.id)}>delete</button>
+            </p>
+            <p>likes:{blog.likes}</p>
+          </div>
+        ))}
+      </>
+    )
   return <h1>Blogs view</h1>
 }
 
