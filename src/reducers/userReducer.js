de52471b53182e01ef285059bdca2bb0c4b4cb72
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-03-11 16:08:13 +0200

    storing userinfo in redux store

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: null,
+  reducers: {
+    setUser(state, action) {
+      return action.payload
+    },
+    removeUser(state, action) {
+      return action.payload
+    },
+  },
+})
+
+export const { setUser, removeUser } = userSlice.actions
+
+export default userSlice.reducer
