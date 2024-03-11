68b9443b6a4f4591132de2cddaae160834a8260d
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-03-11 16:31:19 +0200

    adding updateLikes ,removeBlog async thunk and addBlog action

--- src/reducers/blogReducer.js
+++ src/reducers/blogReducer.js
@@ -1,13 +1,70 @@
 import { createSlice } from '@reduxjs/toolkit'
+import blogService from '../requestService/blog'
+import { setNotification } from './notificationReducer'
 
 const blogSlice = createSlice({
   name: 'blogs',
   initialState: [],
   reducers: {
-    setBlogs(action, payload) {
-      return initialState
+    setBlogs(state, action) {
+      return action.payload
+    },
+    addBlog(state, action) {
+      state.push(action.payload)
+    },
+    updateBlog(state, action) {
+      const id = action.payload.id
+      return state.map((blog) => (blog.id !== id ? blog : action.payload))
+    },
+    deleteBlog(state, action) {
+      const id = action.payload
+      return state.filter((blog) => blog.id !== id)
     },
   },
 })
 
-export const { setBlogs } = reducers.actions
+export const { setBlogs, addBlog, updateBlog, deleteBlog } = blogSlice.actions
+
+export const createBlogs = (blogObject) => {
+  return async (dispatch) => {
+    try {
+      let blog = await blogService.create(blogObject)
+      dispatch(addBlog(blog))
+    } catch (error) {
+      dispatch(setNotification(error.response.data.error))
+      setTimeout(() => {
+        dispatch(setNotification(''))
+      }, 3000)
+    }
+  }
+}
+
+export const updateLikes = (blogObject) => {
+  return async (dispatch) => {
+    try {
+      let blog = await blogService.update(blogObject.id, blogObject)
+      dispatch(updateBlog(blog))
+    } catch (error) {
+      dispatch(setNotification(error.response.data.error))
+      setTimeout(() => {
+        dispatch(setNotification(''))
+      }, 3000)
+    }
+  }
+}
+
+export const removeBlog = (id) => {
+  return async (dispatch) => {
+    try {
+      await blogService.remove(id)
+      dispatch(deleteBlog(id))
+    } catch (error) {
+      dispatch(setNotification(error.response.data.error))
+      setTimeout(() => {
+        dispatch(setNotification(''))
+      }, 3000)
+    }
+  }
+}
+
+export default blogSlice.reducer
