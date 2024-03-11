a889465ff8c3c43ccc1328f3e7bb77b78be6f42d
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-03-11 16:11:05 +0200

    Adding handleLike, handleRemove functionality

--- src/App.js
+++ src/App.js
@@ -7,27 +7,45 @@ import Users from './components/Users'
 import Blogs from './components/Blogs'
 import LoginForm from './components/LoginForm'
 import Footer from './components/Footer'
-import { useUser } from './customHooks/index'
+import BlogForm from './components/BlogForm'
+import { useDispatch } from 'react-redux'
+import { removeUser } from './reducers/userReducer'
+import { useHooks } from './customHooks/index'
+import { createBlogs, updateLikes, removeBlog } from './reducers/blogReducer'
 
 const App = () => {
-  const navigate = useNavigate()
+  const dispatch = useDispatch()
   const [credential, setCredential] = useState(null)
-
-  const user = useUser(credential)
-  console.log(user)
+  const { user, blogs, notification } = useHooks(credential)
 
   const handleLogin = async (credentials) => {
     setCredential(credentials)
-    navigate('/')
   }
 
   const handleLogOut = () => {
-    setCredential(null)
+    dispatch(removeUser(null))
+  }
+
+  const createBlog = async (blogObject) => {
+    dispatch(createBlogs(blogObject))
+  }
+
+  const handleLike = (blogObject) => {
+    const updatedObject = {
+      ...blogObject,
+      likes: blogObject.likes + 1,
+      user: blogObject.user.id,
+    }
+    dispatch(updateLikes(updatedObject))
+  }
+
+  const handleRemove = (id) => {
+    dispatch(removeBlog(id))
   }
 
   return (
     <div>
-      <Notification />
+      <Notification notification={notification} />
       <h2>Bloglist App</h2>
 
       <MenuLink user={user} handleLogOut={handleLogOut} />
@@ -40,7 +58,21 @@ const App = () => {
           path='/login'
           element={<LoginForm handleLogin={handleLogin} />}
         />
-        <Route path='/blogs' element={<Blogs user={user} />}></Route>
+        <Route
+          path='/blogs'
+          element={
+            <Blogs
+              user={user}
+              blogs={blogs}
+              handleLike={handleLike}
+              handleRemove={handleRemove}
+            />
+          }
+        />
+        <Route
+          path='/create'
+          element={user && <BlogForm createBlog={createBlog} />}
+        />
         <Route path='/' element={<Home user={user} />} />
       </Routes>
       <Footer />
