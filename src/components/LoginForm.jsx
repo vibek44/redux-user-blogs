017d433ee48e741e0377e082b19196197087f7f1
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-03-11 16:13:59 +0200

    Adding setTimeout function

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
-import { useDispatch /*useSelector*/ } from 'react-redux'
-import { setNotification } from '../reducers/notificationReducer'
+import { useDispatch } from 'react-redux'
 import { useField } from '../customHooks'
-import { useNavigate } from 'react-router-dom'
+import { setNotification } from '../reducers/notificationReducer'
 
 const LoginForm = ({ handleLogin }) => {
-  const navigate = useNavigate()
   const dispatch = useDispatch()
   const userName = useField('text')
   const passWord = useField('password')
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    if (!(userName.value && passWord.value))
-      return dispatch(setNotification('username or password is missing'))
-    const credentials = {
-      username: userName.value,
-      password: passWord.value,
+    if (!(userName.value && passWord.value)) {
+      dispatch(setNotification('username or password missing'))
+      setTimeout(() => {
+        dispatch(setNotification(''))
+      }, 3000)
+    } else {
+      const credentials = {
+        username: userName.value,
+        password: passWord.value,
+      }
+      handleLogin(credentials)
     }
-    handleLogin(credentials)
   }
 
   return (
